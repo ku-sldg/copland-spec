c3d676eb1e092b85676805292cfdfc7d18820981
Will Thomas <30wthomas@gmail.com>
2025-03-26 17:06:49 -0500

    Adapting to rocq-candy 0.2.2, adding some eqclasses for terms

--- theories/Term_Defs_Core_Typeclasses.v
+++ theories/Term_Defs_Core_Typeclasses.v
@@ -2,7 +2,7 @@ From RocqCandy Require Import All.
 
 From CoplandSpec Require Import Term_Defs_Core Prelude.
 
-Global Instance EqClass_ASP_PARAMS : EqClass ASP_PARAMS.
+Global Instance EqClass_ASP_PARAMS `{EqClass json} : EqClass ASP_PARAMS.
 refine (Build_EqClass _ 
   (fun '(asp_paramsC a1 la1 p1 t1) '(asp_paramsC a2 la2 p2 t2) => 
     (eqb a1 a2) && (eqb la1 la2) && (eqb p1 p2) && (eqb t1 t2)
@@ -11,6 +11,56 @@ refine (Build_EqClass _
 ).
 eq_crush.
 Defined.
+
+Global Instance EqClass_ASP `{EqClass ASP_PARAMS, EqClass Plc}: EqClass ASP.
+refine (Build_EqClass _ 
+  (fun a1 a2 =>
+    match a1, a2 with
+    | NULL, NULL => true
+    | (ASPC par1), (ASPC par2) => eqb par1 par2
+    | SIG, SIG => true
+    | HSH, HSH => true
+    | APPR, APPR => true
+    | ENC p1, ENC p2 => eqb p1 p2
+    | _, _ => false
+    end
+  ) 
+  (fun a1 a2 => _)
+).
+destruct a1, a2; ff; eq_crush.
+Defined.
+
+Global Instance EqClass_SP : EqClass SP.
+refine (Build_EqClass _
+  (fun s1 s2 =>
+    match s1, s2 with
+    | ALL, ALL => true
+    | NONE, NONE => true
+    | _, _ => false
+    end
+  )
+  (fun s1 s2 => _
+  )
+).
+destruct s1, s2; ff.
+Defined.
+
+Fixpoint eqb_Term `{EqClass Plc, EqClass SP, EqClass ASP} (t1 t2 : Term) : bool :=
+  match t1, t2 with
+  | asp a1, asp a2 => eqb a1 a2
+  | att p1 t1, att p2 t2 => eqb p1 p2 && eqb_Term t1 t2
+  | lseq t1 t2, lseq t1' t2' => eqb_Term t1 t1' && eqb_Term t2 t2'
+  | bseq s t1 t2, bseq s' t1' t2' => 
+      eqb s s' && eqb_Term t1 t1' && eqb_Term t2 t2'
+  | bpar s t1 t2, bpar s' t1' t2' => 
+      eqb s s' && eqb_Term t1 t1' && eqb_Term t2 t2'
+  | _, _ => false
+  end.
+
+Global Instance EqClass_Term `{EqClass Plc, EqClass SP, EqClass ASP} : EqClass Term.
+refine (Build_EqClass _ eqb_Term (fun t1 => _ )).
+induction t1; intros y; destruct y; simpl in *; ff; eq_crush; rw_all.
+Qed.
 
 Fixpoint eqb_EvidenceT `{EqClass N_ID, EqClass ASP_PARAMS, EqClass Plc} (e1 e2 : EvidenceT) : bool :=
   match e1, e2 with
@@ -27,8 +77,5 @@ Fixpoint eqb_EvidenceT `{EqClass N_ID, EqClass ASP_PARAMS, EqClass Plc} (e1 e2 :
 
 Global Instance EqClass_EvidenceT `{EqClass N_ID, EqClass Plc, EqClass ASP_PARAMS} : EqClass EvidenceT.
 eapply Build_EqClass with (eqb := eqb_EvidenceT).
-induction x, y; simpl in *; ff; eq_crush;
-try erewrite IHx in *; ff;
-try erewrite IHx1 in *;
-try erewrite IHx2 in *; ff.
-Defined.
+induction x, y; simpl in *; ff; eq_crush; rw_all.
+Qed.
