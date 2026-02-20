9a2544b893836ca9d4aa5eba46ae4d3a0164b7d7
Will Thomas <30wthomas@gmail.com>
2026-02-19 23:02:07 -0600

    `do_appraisal_summary` proven and correct for most part it seems.

--- theories/TypeSys.v
+++ theories/TypeSys.v
@@ -2345,7 +2345,7 @@ Proof.
     ff; repeat (rewrite plug_compose); ff.
 Defined.
 
-Inductive provenance (G : GlobalContext) : EvidenceT -> EvidenceT -> Prop :=
+Inductive provenance (G : GlobalContext) : EvidenceT -> EvidenceT -> Type :=
   | prov_refl : forall e, provenance G e e
   | prov_left_inj : forall e e',
       provenance G e e' ->
@@ -2385,9 +2385,9 @@ Lemma provenance_trans : forall G e1 e2 e3,
   provenance G e1 e3.
 Proof.
   intros.
-  prep_induction H0.
-  induction H0; ff; prover;
-  invc H; prover.
+  prep_induction X0.
+  induction X0; ff; prover;
+  invc X; prover.
 Qed.
 Local Hint Resolve provenance_trans : prov.
 
@@ -2412,7 +2412,32 @@ Theorem provenance_monotonic : forall G e e',
   EvidenceT_depth e <= EvidenceT_depth e'.
 Proof.
   intros.
-  induction H; ff with l.
+  induction X; ff with l.
+Qed.
+
+Definition EvidenceT_size : EvidenceT -> nat :=
+  fix F e :=
+  match e with
+  | mt_evt => 0
+  | nonce_evt _ => 1
+  | asp_evt _ _ e' => 1 + F e'
+  | left_evt e' => 1 + F e'
+  | right_evt e' => 1 + F e'
+  | split_evt e1 e2 => 1 + (F e1) + (F e2)
+  end.
+
+Lemma EvidenceT_depth_le_size : forall e,
+  EvidenceT_depth e <= EvidenceT_size e.
+Proof.
+  induction e; ff with l.
+Qed.
+
+Theorem provenance_mono_or_refl : forall G e e',
+  provenance G e e' ->
+  { e = e' } + { EvidenceT_size e < EvidenceT_size e' }.
+Proof.
+  intros.
+  induction X; ff with l.
 Qed.
 
 Theorem provenance_antisym : forall G e e',
@@ -2421,12 +2446,10 @@ Theorem provenance_antisym : forall G e e',
   e = e'.
 Proof.
   intros.
-  prep_induction H.
-  induction H; ff;
-  try (invc H0; ff with a;
+  prep_induction X.
+  induction X; ff;
+  try (invc X0; ff with a;
     repeat (find_eapply_lem_hyp provenance_monotonic); ff with l; fail).
-  invc H1; ff with a;
-  repeat (find_eapply_lem_hyp provenance_monotonic); ff with l.
 Qed.
 
 Theorem typecheck_preserves_provenance : forall G t p e e',
@@ -3389,6 +3412,115 @@ Qed.
 Definition flatten_appraisal_summary (s : AppraisalSummary) : RawEv :=
   appify s.
 
+Lemma no_tc_nonce : forall G p e n,
+  typeof G p e (asp APPR) (nonce_evt n) ->
+  False.
+Proof.
+  intros.
+  prep_induction X.
+  induction X; normer; normer.
+Qed.
+
+Lemma no_tc_left : forall G p e e',
+  typeof G p e (asp APPR) (left_evt e') ->
+  { False } + { normalize_ev G (left_evt e') = mt_evt }.
+Proof.
+  intros.
+  prep_induction X.
+  induction X; normer.
+Qed.
+
+Lemma no_tc_right : forall G p e e',
+  typeof G p e (asp APPR) (right_evt e') ->
+  { False } + { normalize_ev G (right_evt e') = mt_evt }.
+Proof.
+  intros.
+  prep_induction X.
+  induction X; normer.
+Qed.
+
+Inductive appr_unwrap_chain (G : GlobalContext) (p : Plc) : EvidenceT -> EvidenceT -> Type :=
+| auc_refl : forall e, 
+    appr_unwrap_chain G p e e
+| auc_step : forall e e_next e_inner p' aid args e' appr_id attrs attrs' nv,
+    normalize_ev G e = asp_evt p' (asp_paramsC aid args) e' ->
+    (asp_types G) ![ appr_id ] = Some (ev_arrow UNWRAP attrs) ->
+    (asp_comps G) ![ aid ] = Some appr_id ->
+    (asp_types G) ![ aid ] = Some (ev_arrow (WRAP nv) attrs') ->
+    e_next = asp_evt p (asp_paramsC appr_id args) e ->
+    appr_unwrap_chain G p e_next e_inner ->
+    appr_unwrap_chain G p e e_inner.
+
+Lemma appr_unwrap_chain_measure_decreases : forall G p e e_inner,
+  appr_unwrap_chain G p e e_inner ->
+  EvidenceT_depth (normalize_ev G e_inner) <= EvidenceT_depth (normalize_ev G e).
+Proof.
+  intros.
+  induction X; ff with l; normer.
+Qed.
+
+Lemma tc_split_eventually_resolves : forall G p e et1 et2,
+  typeof G p e (asp APPR) (split_evt et1 et2) ->
+  { e_inner & 
+    ((appr_unwrap_chain G p e e_inner) *
+    (
+      (* Case 1: The chain terminates via tc_appr_asp_extend *)
+      { '(aid, p', args, e', nv, isig, attrs', appr_id, fwd, attrs) &
+        (normalize_ev G e_inner = asp_evt p' (asp_paramsC aid args) e') *
+        ((asp_types G) ![ aid ] = Some (ev_arrow (EXTEND nv isig) attrs')) *
+        ((asp_comps G) ![ aid ] = Some appr_id) *
+        ((asp_types G) ![ appr_id ] = Some (ev_arrow fwd attrs)) * (fwd <> UNWRAP) *
+        (et1 = asp_evt p (asp_paramsC appr_id args) e_inner) *
+        (typeof G p e' (asp APPR) et2)
+      }
+      +
+      (* Case 2: The chain terminates via tc_appr_split *)
+      { '(el, er) & 
+        (normalize_ev G e_inner = split_evt el er) *
+        (typeof G p (left_evt e_inner) (asp APPR) et1) *
+        (typeof G p (right_evt e_inner) (asp APPR) et2)
+      }
+    ))%type
+  }.
+Proof.
+  intros.
+  prep_induction X.
+  induction X; try (normer; fail).
+  - repeat (normer).
+
+  (* Case: tc_appr_asp_unwrap_wrap (The Recursive Looper) *)
+  - (* The IH gives us the resolved inner evidence and the rest of the chain *)
+    ff.
+    edestruct (IHX _ _ eq_refl eq_refl) as [e_inner [Hchain Hcases]]; eauto.
+    exists e_inner.
+    split.
+    + (* Prepend the current unwrap step to the chain *)
+      eapply auc_step; eauto.
+    + (* Pass the resolution cases up directly *)
+      exact Hcases.
+
+  (* Case: tc_appr_asp_extend (Base Creator 1) *)
+  - (* No unwrapping occurred here, so e_inner is just e *)
+    ff.
+    exists e.
+    split.
+    + eapply auc_refl.
+    + left.
+      (* Pack the existentials matching the rule's premises *)
+      exists (aid, p', args, e', (exist _ n nlt), isig, attrs', appr_id, fwd, attrs).
+      repeat split; ff.
+
+  (* Case: tc_appr_split (Base Creator 2) *)
+  - (* No unwrapping occurred here, so e_inner is just e *)
+    ff.
+    exists e.
+    split.
+    + eapply auc_refl.
+    + right.
+      exists (el, er).
+      repeat split; eauto.
+Qed.
+
 (* The correctness condition for the appraisal summary:
 all the evidence that appears in the original raw evidence "r" must be
 "accounted for" in the appraisal summary. This means essentially
@@ -3404,7 +3536,7 @@ Equations? do_appraisal_summary (G : GlobalContext) (r : RawEv)
     ( Hty : { et' & typeof G p et' (asp APPR) et })
     ( Hsize : evt_stack_denotation G et (length r) )
     : { s : AppraisalSummary | appr_summary_correct r s } 
-      by wf (EvidenceT_depth et) :=
+      by wf (EvidenceT_depth (normalize_ev G (projT1 Hty))) :=
   do_appraisal_summary G r p mt_evt Hty Hsize := 
     exist (appr_summary_correct r) [] _;
   do_appraisal_summary G r p (nonce_evt nid) Hty Hsize := _;
@@ -3414,88 +3546,110 @@ Equations? do_appraisal_summary (G : GlobalContext) (r : RawEv)
   do_appraisal_summary G r p (split_evt et1 et2) Hty Hsize := _.
 Proof.
   all: subst; unfold appr_summary_correct.
-- invc Hsize.
-  assert (r = []) by (destruct r; ff with l; fail).
-  ff.
-- invc X; normer.
-- invc X; (try (normer; fail)).
-  * (* mt *)
-    destruct asp_params; normer.
-    invc Hsize; normer.
-    invc X.
-    exists [].
+  - invc Hsize.
     assert (r = []) by (destruct r; ff with l; fail).
     ff.
-  * (* nonce *)
-    invc Hsize; normer.
-    exists ([((check_nonce_params), r)]).
-    ff; rewrite app_nil_r; ff.
-  * (* asp : UNWRAP/WRAP *)
-    invc Hsize; normer.
-    eapply do_appraisal_summary; ff with l.
+  - eapply no_tc_nonce in X as ?; ff.
+  - inv X; (try (normer; fail)).
+    * (* mt *)
+      destruct asp_params; normer.
+      invc Hsize; normer.
+      invc X0.
+      exists [].
+      assert (r = []) by (destruct r; ff with l; fail).
+      ff.
+    * (* nonce *)
+      invc Hsize; normer.
+      exists ([((check_nonce_params), r)]).
+      ff; rewrite app_nil_r; ff.
+    * 
+      eapply (typeof_norm_proper G _ _ e') in X0 as ? > [
+        | eapply normalize_ev_done in H as ?; normer
+      ].
+      destruct X1 as [ evv Htyev Hnev ].
+      eapply evt_stack_denotation_transfer in Hsize as ? > [
+        | eapply Hnev
+      ].
+      eapply (do_appraisal_summary G r p evv (existT _ e' Htyev) X1).
+      ff with l.
+      assert (normalize_ev G e' = e'). {
+        eapply normalize_ev_done in H as ?; normer.
+      }
+      eapply normalize_ev_measure_decrease in H as ?.
+      ff with l.
+    * 
+      destruct fwd; ff.
+      + (* outer is REPLACE *)
+        invc Hsize; normer.
+        exists ([((asp_paramsC aid args), r)]).
+        ff; rewrite app_nil_r; ff.
+      + (* outer is WRAP - equivalent to REPLACE - weird but okay? *)
+        invc Hsize; normer.
+        exists ([((asp_paramsC aid args), r)]).
+        ff; rewrite app_nil_r; ff.
+      + (* outer is extend, inner is replace! *)
+        inv Hsize; normer.
+        destruct (peel_n_rawev n_ext r) as [[l1 l2] |] eqn:Hpeel;
+        try (  find_eapply_lem_hyp peel_n_rawev_none_spec; ff with l).
+        eapply peel_n_rawev_result_spec in Hpeel; ff.
+        rewrite length_app in *.
+        assert (n1 = Datatypes.length l2) by (ff with l); ff.
+        exists ([((asp_paramsC appr_id args), l1)] ++ [((asp_paramsC aid args), l2)]).
+        ff.
+        rewrite app_nil_r.
+        pp Permutation_app.
+        ff.
+  - eapply no_tc_left in X as ?; normer.
+    eapply equiv_preserves_denotation_size_fwd in Hsize.
+    normer; invc Hsize.
+    assert (r = []) by (destruct r; ff with l; fail).
+    exists []; ff.
+  - eapply no_tc_right in X as ?; normer.
+    eapply equiv_preserves_denotation_size_fwd in Hsize.
+    normer; invc Hsize.
+    assert (r = []) by (destruct r; ff with l; fail).
+    exists []; ff.
+  - (* split_evt *)
+    inv Hsize.
+    eapply tc_split_eventually_resolves in X as HS.
+    destruct HS as [e_inner [Hchain [Hextend | Hsplit]]].
 
-    admit.
-  * (* asp : REPLACE *)
-    exists ([((asp_paramsC appr_id args), r)]).
-    ff; rewrite app_nil_r; ff.
-- invc X; normer.
-  eapply equiv_preserves_denotation_size_fwd in Hsize.
-  normer; invc Hsize.
-  assert (r = []) by (destruct r; ff with l; fail).
-  exists []; ff.
-- invc X; normer.
-  eapply equiv_preserves_denotation_size_fwd in Hsize.
-  normer; invc Hsize.
-  assert (r = []) by (destruct r; ff with l; fail).
-  exists []; ff.
-- (* split_evt *)
-  invc Hsize.
-  invc X; normer.
-  * (* EXTEND: recurse *)
+    * (* EXTEND: recurse *)
+      destruct Hextend as [[[[[[[[[[aid p'] args] e'] nv] isig] attrs'] appr_id] fwd] attrs] [[[[[[Hn Haty] Hcomp] Happty] Hfwd] Heveq] Htyv]].
+      destruct (peel_n_rawev s1 r) as [[l1 l2] |] eqn:Hpeel;
+      try (  find_eapply_lem_hyp peel_n_rawev_none_spec; ff with l).
+      eapply peel_n_rawev_result_spec in Hpeel; ff.
+      rewrite length_app in *.
+      assert (s2 = Datatypes.length l2) by (ff with l); ff.
+
+      assert ({ s : AppraisalSummary | appr_summary_correct l2 s }). {
+        eapply (do_appraisal_summary G l2 p et2 (existT _ e' Htyv) X1).
+        ff with l.
+        eapply appr_unwrap_chain_measure_decreases in Hchain.
+        eapply normalize_ev_measure_decrease in Hn as ?. 
+        assert (normalize_ev G e' = e') by (eapply normalize_ev_done in Hn as ?; normer).
+        ff with l.
+      }
+      unfold appr_summary_correct in *.
+      destruct X2 as [l2rw Hl2].
+      exists ([((asp_paramsC aid args), l1)] ++ l2rw).
+      pp Permutation_app.
+      ff.
+    * 
+    destruct Hsplit as [[el er] [[Hn Htyl] Htyr]].
+    eapply appr_unwrap_chain_measure_decreases in Hchain as ?.
     destruct (peel_n_rawev s1 r) as [[l1 l2] |] eqn:Hpeel;
     try (  find_eapply_lem_hyp peel_n_rawev_none_spec; ff with l).
     eapply peel_n_rawev_result_spec in Hpeel; ff.
     rewrite length_app in *.
     assert (s2 = Datatypes.length l2) by (ff with l); ff.
-
-    assert ({ s : AppraisalSummary | appr_summary_correct l2 s }). {
-      eapply do_appraisal_summary; ff with l.
-    }
-    unfold appr_summary_correct in *.
-    destruct X as [l2rw Hl2].
-    exists ([((asp_paramsC aid args), l1)] ++ l2rw).
-    pp Permutation_app.
-    ff.
-  * (* EXTEND: recurse *)
-    destruct (peel_n_rawev s1 r) as [[l1 l2] |] eqn:Hpeel;
-    try (  find_eapply_lem_hyp peel_n_rawev_none_spec; ff with l).
-    eapply peel_n_rawev_result_spec in Hpeel; ff.
-    rewrite length_app in *.
-    assert (s2 = Datatypes.length l2) by (ff with l); ff.
-
-    assert ({ s : AppraisalSummary | appr_summary_correct l2 s }). {
-      eapply do_appraisal_summary; ff with l.
-    }
-    unfold appr_summary_correct in *.
-    destruct X as [l2rw Hl2].
-    exists ([((asp_paramsC aid args), l1)] ++ l2rw).
-    pp Permutation_app.
-    ff.
-  * 
-  pp (existT (fun x' => typeof G p x' (asp APPR) et1) _ X2) as Hty1.
-  pp (existT (fun x' => typeof G p x' (asp APPR) et2) _ X3) as Hty2.
-  destruct (peel_n_rawev s1 r) as [[l1 l2] |] eqn:Hpeel;
-  try (  find_eapply_lem_hyp peel_n_rawev_none_spec; ff with l).
-  eapply peel_n_rawev_result_spec in Hpeel; ff.
-  rewrite length_app in *.
-  assert (s2 = Datatypes.length l2) by (ff with l); ff.
-  destruct (do_appraisal_summary G _ p et1 Hty1 X0 (lt_max_l _ _)) as [S1 Hsum1].
-  destruct (do_appraisal_summary G _ p et2 Hty2 X1 (lt_max_r _ _)) as [S2 Hsum2].
-  exists (S1 ++ S2).
-  unfold appr_summary_correct, flatten_appraisal_summary in *.
-  erewrite appify_app in *.
-  eapply Permutation_app; ff.
-Admitted.
-
-Definition do_AppraisalSummary (et:EvidenceT) (r:RawEv) (G:GlobalContext) : Result AppraisalSummary string := 
-  do_AppraisalSummary' et r G [].
+    eapply normalize_ev_measure_decrease in Hn as ?; ff with l.
+    destruct (do_appraisal_summary G _ p et1 (existT _ (left_evt e_inner) Htyl) X0) as [S1 Hsum1].
+    ff with l; norm; ff with l.
+    destruct (do_appraisal_summary G _ p et2 (existT _ (right_evt e_inner) Htyr) X1) as [S2 Hsum2].
+    ff with l; norm; ff with l.
+    exists (S1 ++ S2).
+    unfold appr_summary_correct, flatten_appraisal_summary in *.
+    erewrite appify_app in *.
+    eapply Permutation_app; ff.
+Defined.
